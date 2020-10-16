10847f858fac51a90862b3e9b06d1a3a4bd2840d
Tabitha Perry <tabitha.perry@generalassemb.ly>
2020-10-15 20:48:19 -0400

    Update bubblesort.js

--- bubblesort.js
+++ bubblesort.js
@@ -1,18 +1,16 @@
 function bubbleSort(array) {
-    // Make some sort of loop that keeps happening if there were any swaps
-    // that happened this turn
+    // Make some sort of loop that keeps happening if there were any swaps that happened this turn (ie. if isSorted is false)
 
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
+    // reset isSorted to true each time so we can detect if a swap happened in this specific iteration.
 
     // Make another loop (inside the first one) to go through one 
     // round of swapping from the start of the list to the end
     
     // Inside inner loop:
     // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
+    // swap them if the current index is greater than the index to the right.
 
-    // Make sure to keep track of whether a swap happened!
+    // Make sure to keep track of whether a swap happened (if the array is sorted)!
 
     // After both loops have exited, remember to return the array
     return array;
